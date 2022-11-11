d126a5ab8aca676178abca732df438f4f277dc7a
Alice <disaster.woman007@gmail.com>
2022-11-11 19:13:08 +0300

    add new compomtents

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Projects from "./Pages/Projects/Projects";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router path="/" exact>
+    <>
+      <Router>
         <Navbar />
-      </Router>  
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
+//    <Route path="/home" element={<Main />} />;
 
 export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Button.scss'
+function Button() {
+  return (
+    <div className="button mt-20">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <span>Contact me</span>
+    </div>
+  );
+}
+
+export default Button
